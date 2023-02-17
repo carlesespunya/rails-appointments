9afd8491f4fc9c41714f46f7d22c1649677f9b14
carles espunya <carlespuna@gmail.com>
2023-02-17 12:31:29 +0100

    db name changed / first migration

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_175500) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
