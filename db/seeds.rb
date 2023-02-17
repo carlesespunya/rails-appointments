9e9c9f3833219af8c4bd3a4dc28e6aa5e20df8e2
carles espunya <carlespuna@gmail.com>
2023-02-17 14:04:16 +0100

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,30 @@
-if User.none?
-  10.times do
-    User.create!(
-      name: Faker::Name.name,
-      email: Faker::Internet.email,
-      password: "1234asdf"
-    )
-  end
+User.destroy_all
+Appointment.destroy_all
+
+User.create!(
+  name: "admin",
+  email: "admin@mail.com",
+  role: 'Patient',
+  city: Faker::Address.city,
+  password: "123123123"
+)
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    role: 'Patient',
+    city: Faker::Address.city,
+    password: "123123123"
+  )
 end
 
-if Article.none?
-  20.times do
-    Article.create!(
-      title: Faker::Commerce.product_name,
-      body: Faker::Lorem.paragraphs(number: 5).join,
-      user: User.all.sample
-    )
-  end
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    role: 'Doctor',
+    city: Faker::Address.city,
+    password: "123123123"
+  )
 end
